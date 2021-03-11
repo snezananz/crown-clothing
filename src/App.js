9e5619dffda8b27687eb7926dacfdb40afb1fabb
snezananz <nz.snezana@gmail.com>
2021-03-11 18:38:25 +1300

    implemented firebase utils, included ability to store authenticated users into firebase database

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 import Header from './components/header/header.component';
-import { auth } from './firebase/firebase.utils';
+import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 
 class App extends React.Component {
 
@@ -21,12 +21,35 @@ class App extends React.Component {
   // method to call when unmounting, to unsubscribe
   unsubscribeFromAuth = null
 
-  componentDidMount(){
-    // onAuthStateChanged() returns a function for unsubscribing!
-      this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
+  componentDidMount() {
+    /* onAuthStateChanged() returns a function for unsubscribing!
+      /*this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
       // authentication state has changed, change the state information
-      this.setState({currentUser: user});
-    }) 
+      this.setState({currentUser: user});*/
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+      
+      if (userAuth) {
+        // Add this user into out users database table, if not already there
+        const userRef = await createUserProfileDocument(userAuth);
+
+        // create a listener to listen for document changes and update state every time; in our case there will be no change just an initial 'update' when the snapshot will be sent to us with the user data, which we will store in the state
+
+        userRef.onSnapshot(snapshot => {
+          
+          this.setState({
+            currentUser: {
+              id: snapshot.id,
+              ...snapshot.data()
+            }
+          }, () => { console.log( this.state ) });
+          // this wont work, setState is async so to accurately display state AFTER update we need to do it in the callback, not in the code below
+          //console.log(this.state);
+        });
+      } else {
+        this.setState({ currentUser: userAuth }); // sets to null when user logs off
+        console.log(this.state);
+      }      
+    });
   }
 
   componentWillUnmount(){
--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -16,6 +16,35 @@ firebase.initializeApp(config);
 export const auth = firebase.auth();
 export const firestore = firebase.firestore();
 
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+    if(!userAuth) return;
+    
+    const userRef = firestore.doc(`users/${userAuth.uid}`);
+    const snapshot = await userRef.get();
+
+    if(!snapshot.exists){
+        // create user
+        const { displayName, email} = userAuth;
+        const createdAt = new Date();
+
+        try{
+            await userRef.set({
+                displayName,
+                email,
+                createdAt,
+                ...additionalData
+            });
+            console.log('Inserted user into the firebase database');
+        } catch(error){
+            console.log('error creating user', error.message);    
+        }
+    } else {
+        //console.log('exists');
+    };
+
+    return userRef;
+};
+
 // authentication
 const provider = new firebase.auth.GoogleAuthProvider();
 provider.setCustomParameters({prompt: 'select_account'});
