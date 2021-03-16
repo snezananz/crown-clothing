032747e933b2b45af998b02768e2f0465a6d47eb
snezananz <nz.snezana@gmail.com>
2021-03-16 14:16:52 +1300

    added cart icon and cart dropdown components, also implemented cart reducer

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import { ReactComponent as Logo } from '../../assets/crown.svg';
 import { connect } from 'react-redux';
 
 import './header.styles.scss';
 import '../../assets/crown.svg';
 import { auth } from '../../firebase/firebase.utils';
+import CartIcon from '../cart-icon/cart-icon.component';
+import CartDropdown from '../cart-dropdown/cart-dropdown.component';
 
-const Header = ({ currentUser }) => (
+const Header = ({ currentUser, hidden }) => (
     <div className='header'>
         <Link to='/' className='logo-container'>
             <Logo className='logo'/>
@@ -19,13 +21,27 @@ const Header = ({ currentUser }) => (
                 (<div className='option' onClick={() => auth.signOut()}>SIGN OUT</div> ) :
                 (<Link className='option' to='/signin'>SIGN IN</Link>
             )}
+            <CartIcon />
         </div>
+        {
+            hidden ? null : <CartDropdown />
+        }
+        
     </div>
 );
 
 // this function maps state to Header parameter (currentUser)
+/*
 const mapStateToProps = state => ({
     currentUser: state.user.currentUser
+}); 
+*/
+
+// Implementing cart: we now require additional object from state (cart: {hidden}), so we will add this to our argument list - and will destructure the state parameter immedately:
+
+const mapStateToProps = ({ user: {currentUser}, cart: {hidden}}) => ({
+    currentUser,
+    hidden
 });
 
 //export default Header;
--- src/redux/user/user.actions.js
+++ src/redux/user/user.actions.js
@@ -1,4 +1,4 @@
-import { UserActionTypes } from './user.types.js';
+import UserActionTypes from './user.types.js';
 
 export const setCurrentUser = user => ({
     type: UserActionTypes.SET_CURRENT_USER,
