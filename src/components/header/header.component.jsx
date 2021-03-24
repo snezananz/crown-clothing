d443bbd9431130c50d041fe87f305e162ae5aef2
snezananz <nz.snezana@gmail.com>
2021-03-24 17:33:17 +1300

    disabled logging for production

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -18,8 +18,8 @@ const Header = ({ currentUser, hidden }) => (
             <Logo className='logo'/>
         </Link>
         <div className='options'>
-            <Link className='option' to='shop'>SHOP</Link>
-            <Link className='option' to='shop'>CONTACT</Link>
+            <Link className='option' to='/shop'>SHOP</Link>
+            <Link className='option' to='/shop'>CONTACT</Link>
             {  currentUser ?
                 (<div className='option' onClick={() => auth.signOut()}>SIGN OUT</div> ) :
                 (<Link className='option' to='/signin'>SIGN IN</Link>
